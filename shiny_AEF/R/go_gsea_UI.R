a4e9c49a507b2042a0dc7b9184e079c214976b56
mmoli <miquel.moli-gonzalez@imean-biotech.com>
2026-05-01 12:25:24 +0200

    adaptations UI go_gsea, choix de l'univers(génome ou RNASeq) non actif (shared functions ?)

--- shiny_AEF/R/go_gsea_UI.R
+++ shiny_AEF/R/go_gsea_UI.R
@@ -7,21 +7,106 @@ go_gsea_ui <- function(id) {
   
   # Interface utilisateur
   tagList(
-    h2("Enrichissement (GO term)"),
-    h3("Méthode GSEA"),
+    h2("Enrichissement (GO terms)"),
+    h3("Méthode ORA"),
     
     #------------------ Paramètres de la GSEA GO term -------------------
     fluidRow(
-      box(title = "Paramètres GSEA", 
-          solidHeader = TRUE,
-          status = "primary",
-          width = 12,
-          selectInput(ns("rank_type_gsea"), "Classement des gènes :",
-                      choices = c("log2FC" = "log2FC",
-                                  "padj"  = "padj")),
-          selectInput(ns("ont"), "Ontologie :",
-                      choices = c("BP"="BP","CC"="CC","MF"="MF")),
-          actionButton(ns("runGSEA"), "Run", icon = icon("play"), class="btn-success")
+      box(
+        title = "Paramètres GSEA", 
+        solidHeader = TRUE,
+        status = "primary",
+        width = 12,
+        
+        fluidRow(
+          box(
+            title = "Classement des gènes",
+            width = 4,
+            status = "info",
+            
+            column(
+              width = 6,
+              radioButtons(
+                ns("rank_type_gsea"), 
+                "Choisir le critère de classement :",
+                choices = c("log2FC" = "log2FC", "padj"  = "padj")),
+              selected = "log2FC"
+              )
+            ),
+            # selectInput(
+            #   ns("rank_type_gsea"), 
+            #   "Choisir le critère de classement :",
+            #   choices = c("log2FC" = "log2FC", "padj"  = "padj")),
+           
+          # paramètres statistiques ORA 
+          box(
+            title = "Paramètres statistiques ORA",
+            width = 8,
+            status = "primary",
+            
+            fluidRow(
+              column(
+                width = 6,
+                selectInput(
+                  ns("ont"),
+                  "Ontologie GO",
+                  choices = c(
+                    "Biological Process (BP)" = "BP",
+                    "Molecular Function (MF)" = "MF",
+                    "Cellular Component (CC)" = "CC"
+                  ),
+                  selected = "BP"
+                ),
+                
+                selectInput(
+                  ns("univers_ora_go"),
+                  "Choix de l'univers",
+                  choices = c(
+                    "Génome de référence (recommandé)" = "gen_ref",
+                    "Gène de l'analyse RNA-seq" = "gene_list"
+                  )
+                )
+              ),
+                
+              column(
+                width = 6,
+                selectInput(
+                  ns("padjust_method_go_ora"),
+                  "Méthode de correction multiple",
+                  choices = c(
+                    "Benjamini-Hochberg (FDR)" = "BH",
+                    "Bonferroni" = "bonferroni",
+                    "Holm" = "holm",
+                    "Benjamini-Yekutieli" = "BY",
+                    "Aucune" = "none"
+                  ),
+                  selected = "BH"
+                ),
+                
+                numericInput(
+                  ns("pval_ora_go"),
+                  "Seuil p-value ORA",
+                  value = 0.05,
+                  min = 0,
+                  max = 0.1,
+                  step = 0.01
+                ) 
+              )
+            )
+          ),
+          
+          fluidRow(
+            column(
+              width = 12,
+              align = "center",
+              actionButton(
+                ns("runGSEA"), 
+                "Run", 
+                icon = icon("play"), 
+                class="btn-success")
+            )
+          )
+        )
       )
     ),
     
